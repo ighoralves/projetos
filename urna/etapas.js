0cfec850062fe635d1b3a24ea97de32647ad6797
IghorAlves <ighoralc@gmail.com>
2022-12-07 08:55:28 -0300

    finalizando js e finalizando o projeto

---
+++ urna/etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "38111",
+        nome: "fulano de tal",
+        partido: "ABC",
+        fotos: [{ url: "38111.jpg", legenda: "Vereador", small: false }],
+      },
+      {
+        numero: "77222",
+        nome: "Beltrano de tal",
+        partido: "DEFG",
+        fotos: [{ url: "77222.jpg", legenda: "Vice-Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "99",
+        nome: "Ciclano",
+        partido: "ABC",
+        vice: "cic",
+        fotos: [
+          { url: "99.jpg", legenda: "Prefeito" },
+          { url: "99-2.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: "84",
+        nome: "Zulano",
+        partido: "QWERTY",
+        vice: "Zul",
+        fotos: [
+          { url: "84.jpg", legenda: "Prefeito" },
+          { url: "84-2.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
